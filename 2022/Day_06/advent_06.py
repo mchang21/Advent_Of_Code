61904c08220e8f69bc9e4af42f8baa9b86f74b79
Melvin Chang <melvchang@gmail.com>
2022-12-06 16:55:24 -0800

    optimized code for day 6

--- 2022/Day_06/advent_06.py
+++ 2022/Day_06/advent_06.py
@@ -7,54 +7,29 @@ def parse_data(file):
 
 datastream = parse_data(PATH + 'input06.txt')
 
-# part one
-def process_packet(datastream):
+# part one and two
+def process_datastream(datastream, max_len):
     window = dict()
+    start = 0
 
-    # initial window
-    for i in range(3):
-        window[datastream[i]] = window.get(datastream[i], 0) + 1
+    for end in range(len(datastream)-max_len):
+        # check if the index is a start-of marker
+        if len(window) == max_len:
+            return end
 
-    for i in range(3, len(datastream)):
-        # add the letter at i
-        window[datastream[i]] = window.get(datastream[i], 0) + 1
-
-        # check if the index is a start-of-packet marker
-        if len(window) == 4:
-            return i+1
+        # add letter
+        window[datastream[end]] = window.get(datastream[end], 0) + 1
 
         # remove letter from the beginning of the window
-        if window[datastream[i-3]] == 1:
-            del window[datastream[i-3]]
-        else:
-            window[datastream[i-3]] -= 1
+        if end-start >= max_len:
+            if window[datastream[start]] == 1:
+                del window[datastream[start]]
+            else:
+                window[datastream[start]] -= 1
+            start += 1
 
-print(f"{process_packet(datastream)} characters need to be processed before the first start-of-packet marker is detected")
+print(f"{process_datastream(datastream, 4)} characters need to be processed before the first start-of-packet marker is detected")
 # Output: 1080 characters need to be processed before the first start-of-packet marker is detected
 
-# part two
-def process_message(datastream):
-    window = dict()
-
-    # initial window
-    for i in range(13):
-        window[datastream[i]] = window.get(datastream[i], 0) + 1
-
-    for i in range(13, len(datastream)):
-        # add the letter at i
-        window[datastream[i]] = window.get(datastream[i], 0) + 1
-
-        # check if the index is a start-of-packet marker
-        if len(window) == 14:
-            return i+1
-
-        # remove letter from the beginning of the window
-        if window[datastream[i-13]] == 1:
-            del window[datastream[i-13]]
-        else:
-            window[datastream[i-13]] -= 1
-
-    return
-
-print(f"{process_message(datastream)} characters need to be processed before the first start-of-message marker is detected")
+print(f"{process_datastream(datastream, 14)} characters need to be processed before the first start-of-message marker is detected")
 # Output: 3645 characters need to be processed before the first start-of-message marker is detected
